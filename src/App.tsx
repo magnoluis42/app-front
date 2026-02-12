fac468965d863f89e1ec378ea225e0f3c3d6d06d
magno <magno.luis42@hotmail.com>
2026-02-12 19:23:09 -0300

    feat: adição de novas rotas

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './features/Home/Home'
 import Order from './pages/Order/Order'
 import Profile from './pages/Profile/Profile'
+import OrderDetail from './pages/OrderDetail/OrderDetail'
+import Cart from './pages/Cart/Cart'
+import Checkout from './pages/Checkout/Checkout'
 
 function App() {
 
@@ -12,6 +15,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/pedidos" element={<Order />} />
         <Route path="/perfil" element={<Profile />} />
+        <Route path="/pedidos/:id" element={<OrderDetail />} />
+        <Route path="/carrinho" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
       </Routes>
     </BrowserRouter>
   )
